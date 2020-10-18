827e6080f6ac9795a41340ff448f18ed1d064918
Sadiqali Zaveri <sadiqalizaveri@gmail.com>
2020-10-18 11:58:22 +0530

    ScrollTop by default on route change Remove unwated env file

--- API/Basic/index.js
+++ API/Basic/index.js
@@ -1,14 +1,16 @@
-import env from '../../env'
+/* global window */
+/* eslint-disable no-underscore-dangle */
 
-module.exports = {
-	getBaseUrl: () => {
-		const protocol = env.PROTOCOL === 'HTTP2' ? 'https://' : 'http://'
-		switch (env.TIER) {
-			case 'production':
-				return 'https://zavtin.com/'
-			case 'dev':
-			default:
-				return `${protocol}localhost:3000/`
-		}
-	},
+export const getBaseUrl = () => {
+	const protocol =
+		window.__ENV__.PROTOCOL === 'HTTP2' ? 'https://' : 'http://'
+	switch (window.__ENV__.TIER) {
+		case 'production':
+			return 'https://zavtin.com/'
+		case 'dev':
+		default:
+			return `${protocol}localhost:3000/`
+	}
 }
+
+export default null
--- env.js
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line no-undef, no-underscore-dangle
-export default typeof window !== 'undefined' ? window.__ENV__ : process.env
